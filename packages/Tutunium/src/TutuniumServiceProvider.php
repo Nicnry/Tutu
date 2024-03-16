7108f438164ea840c5aa1f20a4b4ba9e6aa06080
Nicolas <m.nicolas.henry@gmail.com>
2024-03-16 13:56:56 +0100

    Create custom package for wrapping

---
+++ packages/Tutunium/src/TutuniumServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tutunium;
+
+use Illuminate\Support\ServiceProvider;
+
+class TutuniumServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+    }
+
+    public function register()
+    {
+
+    }
+}
